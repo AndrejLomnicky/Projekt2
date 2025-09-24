bf5a54f4d50be8d40ff9c42f00ae0338cc89f1f7
AndrejLomnicky <lomnicky.andrej@gmail.com>
2025-09-24 09:16:45 +0200

    ahoj

--- Trojuholnik.py
+++ Trojuholnik.py
@@ -11,16 +11,16 @@ if (a**2+b**2==c**2) or (a**2+c**2==b**2) or (b**2+c**2==a**2):
     print("je to pravouhly trojuholnik")
     o = (a+b+c)
     po= o/2
-    S=(po*(po-a)*(po-b)*(po-c)))**0.5
+    S=(po*(po-a)*(po-b)*(po-c))**0.5
     va = 2*S/a
-    vb=2*Sb
-    vc=2*Sc
+    vb=2*S/b
+    vc=2*S/c
     p_vpis=S/po
     p_opis1=(a*b*c)/(4*S)
             #pocitam uhly
-     alpha = round(math.degrees(math.acos((b**2+c**2-a**2)/(2*b*c))),2)
-     beta = round(math.degrees(math.acos((a**2+c**2-b**2)/(2*a*c))),2)
-     gama = round(math.degrees(math.acos((a**2+b**2-c**2)/(2*b*c))),2)
+     alpha = round(math.degrees(math.acos((b**2+c**2-a**2)/(2*b*c)),2))
+     beta = round(math.degrees(math.acos((a**2+c**2-b**2)/(2*a*c)),2))
+     gama = round(math.degrees(math.acos((a**2+b**2-c**2)/(2*b*c)),2))
 print("obvod je:",o,"obsah je:",S,"po je:",po,"vyska na stranu va je:",va,)
 
 
